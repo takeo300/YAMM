f34dab57788430ec18d12a7a49e27981bf6042f3
Max Beikirch <maxnet@onlinehome.de>
2012-03-17 00:16:00 +0100

    update

--- src/bindings/skype/main.cpp
+++ src/bindings/skype/main.cpp
@@ -1,3 +1,11 @@
 #include "main.h"
 
 // DLL-Code here
+
+bool init(std::string username, std::string password, void (error*)(std::string msg))
+{
+    #ifdef windows
+
+    #endif
+    return false;
+}
--- src/bindings/skype/main.h
+++ src/bindings/skype/main.h
@@ -1,4 +1,31 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
+#include <vector>
+
+namespace YAMM
+{
+// start up daemons
+/// @arg error is a callback to a function taking string returning void, to output error messages etc.
+/// @arg receiveMessage is a callback to a function taking string returning void, to output error messages etc.
+bool init(std::string username, std::string password, void (error*)(std::string msg), void (receiveMessage*)(std::string user, std::string msg));
+
+// personal information
+
+struct person
+{
+    std::string firstName, lastName;
+    std::string phoneNumber;
+
+} getPersonalInformation();
+
+// person's environment
+vector<string> getPersonsFriends();
+
+// interaction with other users
+bool sendMessage(std::string user, std::string message);
+
+// shut down daemons
+bool finalize();
+}
 #endif // __MAIN_H__
